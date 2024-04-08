c39b875c4b42a702bc95c821f5f09c771fef7ac7
Alessandro Mathews <Alessandromathews10@gmail.com>
2024-04-08 10:48:32 -0300

    Modificações

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -5,12 +5,10 @@ import image from "../Assets/image.jpg"
 const Sobre = () => {
     return(
         
-        <div className="sobre-container">
+        <body className="sobre-container">
         <h1 className="Sobre-title">Sobre</h1>
-            <div>
-                <img className="image" src={image} alt="Teste"></img>
-            </div>
-        </div>
+        <img className="image" src={image} alt="Teste"></img>
+        </body>
     )
 }
 
