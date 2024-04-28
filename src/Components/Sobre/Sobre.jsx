65299e960e5dccbae2913f826176048c671adadc
Alessandro Mathews <Alessandromathews10@gmail.com>
2024-04-28 19:47:29 -0300

    Alterações;

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -7,7 +7,16 @@ const Sobre = () => {
         
         <body className="sobre-container">
         <h1 className="Sobre-title">Sobre</h1>
-        <img className="image" src={image} alt="Teste"></img>
+        <div className="container-img"><img className="image" src={image} alt="Teste"></img> </div>
+            <div className="texto1">
+        <p>O aplicativo foi desenvolvido com o intuito de reportar denúncias de problemas rodoviários para os responsáveis, visando a validação da informação e a possível resolução para o problema.</p>
+        <div className="texto2">
+        <p>Grupo e funções:</p>
+        <p>Front: Eduardo Marinho, Guilherme e João.</p>
+        <p>Back: Kawã e Eduardo Oliveira.</p>
+        <p>QA: Alessandro, Paulo e Júlia.</p>
+        </div>
+            </div>
         </body>
     )
 }
